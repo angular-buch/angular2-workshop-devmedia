e409de4e20c65c45e9314d5b349b479aa61b1b2d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-11-21 15:59:48 +0100

    {{ interpolation }}

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  books: string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.books = ['Angular 2', 'Angular JS 1'];
+  }
+
+}
