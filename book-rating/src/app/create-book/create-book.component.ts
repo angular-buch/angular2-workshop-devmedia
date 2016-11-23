03a1fecfaa4bbba9ae2facdda99f05dab01c970b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-11-23 11:03:45 +0100

    this.form.reset

--- book-rating/src/app/create-book/create-book.component.ts
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -1,4 +1,5 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Book } from '../shared/book';
 
 @Component({
@@ -8,11 +9,14 @@ import { Book } from '../shared/book';
 })
 export class CreateBookComponent {
 
+  @ViewChild(NgForm) form;
+
   book = Book.empty();
   @Output() bookCreated = new EventEmitter<Book>();
 
   add() {
     this.bookCreated.emit(this.book);
     this.book = Book.empty();
+    this.form.reset(Book.empty());
   }
 }
