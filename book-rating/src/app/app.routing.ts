91f7c856e97339932c96aaa03caddae09126f7b3
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-11-23 15:45:14 +0100

    routing

---
+++ book-rating/src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
+const APP_ROUTES: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'book/:isbn', component: BookDetailsComponent }
+  // { path: '**', component: NotFoundComponent }
+];
+
+export const AppRoutingModule = RouterModule.forRoot(APP_ROUTES);
